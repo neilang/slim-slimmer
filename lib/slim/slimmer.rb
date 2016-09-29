233cda9395025a9eb9ad1e6fed2b70242dadad5c
neilang <neilang@gmail.com>
2016-09-30 07:12:40 +1000

    New gem!

---
+++ lib/slim/slimmer.rb
@@ -0,0 +1,7 @@
+require "slim/slimmer/version"
+
+module Slim
+  module Slimmer
+    # Your code goes here...
+  end
+end
---
+++ lib/slim/slimmer/version.rb
@@ -0,0 +1,5 @@
+module Slim
+  module Slimmer
+    VERSION = "0.1.0"
+  end
+end
